25c3a2e4dfbc83b1b634b33084f5358adf82ad06
MaksymMediuh <maksim2003mmm15@gmail.com>
2022-07-17 16:40:22 +0300

    Done

---
+++ JS/index.js
@@ -0,0 +1,33 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += " active";
+}
+
+var timer = 0;
+ makeTimer(); //Создаем интервал 
+ function makeTimer(){
+    clearInterval(timer) //Очистим интервал, это позволит прервать его работу и отменить перелистывание
+    timer = setInterval(function(){
+      slideIndex++;
+      showSlides(slideIndex);
+    },5000);
+  }
